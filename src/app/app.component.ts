69b98e0a5c77169813c75bd0eb6ff9792b096c84
msuarez <miguel.suarez.ext@nokia.com>
2019-01-14 10:49:24 -0600

    Se agrega uso de uppercase y lowercase, tambien el slice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'pipes';
+  title = "pipes";
+  nombre = "Miguel";
+  arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 }
